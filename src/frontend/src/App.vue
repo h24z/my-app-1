28a0e441adfb8f2d6c903e677bba56801a5ce1e7
h24z <agusty91@gmail.com>
2022-08-17 13:31:09 +0700

    Edited frontend landing page

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,31 +1,27 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div id="app">
+        <HelloThere :msg="msg"/>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import axios      from 'axios'
+import HelloThere from './components/HelloThere.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloThere
+    },
+    data () {
+        return {
+            msg: null
+        }
+    },
+    mounted () {
+        axios
+            .get('http://backend.demo.test/api/hello-there')
+            .then(response => (this.msg = response.data))
+    }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
